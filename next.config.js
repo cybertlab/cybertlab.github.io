6009925c169c9237f87c896a1942182bb702b767
orzmeta <orzmeta@protonmail.com>
2021-12-23 01:28:58 +0800

    fuck js, fuck next.js

--- next.config.js
+++ next.config.js
@@ -12,6 +12,11 @@ module.exports = withBundleAnalyzer({
   // fuck js: fuck next.js
   assetPrefix: '/website/',
 
+  // fuck js: fuck next.js
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    staticFolder: '/website/public',
+  },
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
